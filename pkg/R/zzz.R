e6a6f73cda2356fd5e63ca0a077fa610477d8ad4
yneol (R2STATS ) <yvonnick.noel@uhb.fr>
2012-11-21 15:29:44 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/atelier@25 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/zzz.R
+++ pkg/R/zzz.R
@@ -1,6 +1,15 @@
 .onAttach = function(...) {
 
-   options(guiToolkit="RGtk2")
-   AtelieR()
+  options(guiToolkit="RGtk2")
+  
+  # Create gtkrc file and select Windows theme if necessary
+  # (Default Gnome theme is ugly under Win)
+  if(.Platform$OS.type == "windows") {
+    rc.path = paste(Sys.getenv("R_HOME"),"library/RGtk2/gtk/i386/etc/gtk-2.0",sep="/")
+    rc.file = paste(rc.path,"gtkrc",sep="/")
+    if(file.exists(rc.path) && !file.exists(rc.file)) writeLines("gtk-theme-name=\"MS-Windows\"",rc.file)
+  }
+
+  AtelieR()
 }
 
